2edc3cbb34f37bc47eb54aca9b10ff5f442411f0
nicoo <nicoo@LAPTOP-CGDIFHI0>
2022-03-05 19:16:10 +0100

    Commit de prueba

---
+++ PracticaSI/src/NodoHuffman.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PSI;
+
+/**
+ *
+ * @author nicoo
+ */
+public class NodoHuffman {
+    
+    int frecuencia;
+    char caracter;
+  
+    NodoHuffman left;
+    NodoHuffman right;
+}
